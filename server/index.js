2bae137bf2d69bfe5524f95092acf5d130bfb848
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-04-30 14:15:30 +0300

    server index draft

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(cors());
+app.get('/', async (req, res) => {
+  let ans = await main();
+  res.json(ans);
+});
+app.listen(3000, () => console.log('Server started on the port 3000'));
 
 const prisma = new PrismaClient();
 
-async function main() {}
-
-main().then(async () => {
-  await prisma.$disconnect();
-});
+async function main() {
+  const usersWithRelatedRecords = await prisma.users.findMany({
+    include: {
+      students: true,
+      Parents: true,
+      teacher: true,
+    },
+  });
+  // console.log(usersWithRelatedRecords);
+  return usersWithRelatedRecords;
+}
